57b5a40031370b74797f4b2d900a3fb901149e56
dnet <no@mail.com>
2025-10-14 23:03:10 -0300

    - Agregado detalle relacionado a LoginModel.

--- app/src/main/java/com/sefmat/picorlose/model/LoginModel.kt
+++ app/src/main/java/com/sefmat/picorlose/model/LoginModel.kt
@@ -1,2 +1,12 @@
 package com.sefmat.picorlose.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class LoginModel {
+    // Nombre de Usuario
+    var username by mutableStateOf("")
+    // Contraseña
+    var passwrd by mutableStateOf("")
+}
--- app/src/main/java/com/sefmat/picorlose/repository/LoginRepository.kt
+++ app/src/main/java/com/sefmat/picorlose/repository/LoginRepository.kt
@@ -1,2 +1,7 @@
 package com.sefmat.picorlose.repository
 
+import com.sefmat.picorlose.model.LoginModel
+
+class LoginRepository {
+    private var login = LoginModel()
+}

d4925e1697cec32948b317d253e3cdc32c6105c9
dnet <nuhuh@mail.com>
2025-11-21 14:16:25 -0300

    - Pequeña actualizacion a PictureModel

--- app/src/main/java/com/sefmat/picorlose/data/model/PictureModel.kt
+++ app/src/main/java/com/sefmat/picorlose/data/model/PictureModel.kt
@@ -3,9 +3,8 @@ package com.sefmat.picorlose.data.model
 data class PictureModel(
     val userId: Int,
     val picId: Int,
-    val title: String/*,
-    val picture:*/
-    // COMO IMPLEMENTAR LA IMAGEN?????
+    val title: String,
+    val picture: String // BASE64
 
     // SERAN LAS IMAGENES DE LA TEMATICA QUE HAYAN SIDO PUBLICADAS DURANTE EL DIA
 )
